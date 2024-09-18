547d96cf9c17a9508308f75a0b8c88bea5c12ea9
elliefonua <ellie.fonua@ucas-edu.net>
2024-09-18 18:39:23 +0000

    financial calculator in py

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h> // Include math library
 
 int main(void)
 {
@@ -14,7 +13,7 @@ int main(void)
     printf("%d", (int)e * (int)b / (int)12 + (int)72 / (int)8 - (int)9);
     printf("%d", (int)(17 - (int)e / (int)a) + (int)b * (int)d);
     printf("%d", (int)-2 * (int)(1 * (int)b - (int)a / (int)a) + (int)(e + (int)a - (int)d));
-    printf("%d", (int)-1 * (int)((3 - (int)b * (int)7) / (int)c) - (int)(a * (int)24 / (int)e));
+    printf("%d", (int)-1 * (int)((3 - (int)b * (int)7) / (int)c) - (int)(a * (int)24 / (int)e)));
     printf("%d", (int) (d * (int)c ^ (int)a / (int)15) - (int) (c - (int)a ^ (int)a));
     printf("%d", (int) (1 ^ (int)b * (int)a ^ (int)a + (int)d ^ (int)d) - (int)a ^ (int)c / (int)b);
     printf("%d", (int)(22 / (int)a - (float)2.5) ^ (int)a + (int)(b - (int)e / (int)e) ^ (int)a);
