eba500bad63638f29fd99f95ef72c70532962eb4
elliefonua <ellie.fonua@ucas-edu.net>
2024-09-20 19:26:54 +0000

    silly sentence in py

--- strings/sillysentcs.py
+++ strings/sillysentcs.py
@@ -1,2 +1,7 @@
-#Ellie fonua
-noun = 
+print("Name an animal:")
+animal = input()
+print("Name a present verb:")
+verb = input()
+print("Name a planet:")
+planet = input()
+print("The" , animal + " had secret powers and went" , verb + " on" , planet + " plotting against the evil squirrel union.")
