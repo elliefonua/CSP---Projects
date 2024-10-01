eecc687bab11c9e199ee09de3fda58d6c6e31d19
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-01 19:17:25 +0000

    if statements in py

---
+++ conditionals/oldEnough.c
@@ -0,0 +1,4 @@
+print("How old are you?\n")
+
+if age >= 18:
+    print("You are old enought to vote!")
---
+++ conditionals/oldEnough.py
@@ -0,0 +1,26 @@
+age = int(input("how old are you?\n"))
+
+if age >= 18:
+    print("You are old enough to vote!")
+
+elif age < 18:
+    print("You're not old enough to vote!")
+
+if age >= 16:
+    print("You are old enough to drive!")
+
+elif age < 16:
+    print("You're not old enough to drive!")
+
+if age >= 15:
+    print("You are old enough to get a leaners permit!")
+
+elif age < 15:
+    print("You're not old enough to get a learners permit!")
+
+if age >= 5:
+    print("You are old enough to go to school!")
+
+elif age < 5:
+    print("You're not old enough to go to school!")
+    
