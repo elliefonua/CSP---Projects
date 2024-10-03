79b1823a73a374ac98fb8b198f010275d86a11b0
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-03 19:21:09 +0000

    old enough conditional in py

--- conditionals/oldEnough.py
+++ conditionals/oldEnough.py
@@ -1,10 +1,6 @@
 age = int(input("how old are you?:\n"))
 
 if age >= 18:
-    print("You are old enough to vote, drive, have a leaners permit, and go to school!")
-elif age <= 5:
-    print("You're not old enough to vote, you're not old enough to drive, you're not old enough to have a leaners permit, and you can go to school!")
-elif age <= 15:
-    print("You're not old enough to vote, you're not old enough to drive, you're old enough to have a leaners permit, and you can go to school!")
-elif age <= 17:
-    print("You're not old enough to vote, you're old enough to drive, you're old enough to have a leaners permit, and you can go to school!")
+    print("you are old enough to vote!")
+elif age < 18:
+    print("you're not old enough to vote!")
