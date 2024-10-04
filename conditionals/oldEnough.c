b1d59880ef54d62b5b2c7a04d1270b9025c9ba7d
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-04 02:51:37 +0000

    old enough! in c

--- conditionals/oldEnough.c
+++ conditionals/oldEnough.c
@@ -11,8 +11,10 @@ int main(void){
         printf("you are old enough to drive!\n");
     }else if (age >= 15){
         printf("you are old enough to have a learners permit!\n");
-    }else{
+    }else if (age > 4){
         printf("you are old enough to go to school!\n");
+    }else{
+        printf("you are not old enough to do anything!\n");
     }
     return 0;
 }
