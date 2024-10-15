cf2815bb74772171f0677149459ab9be25417831
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-15 18:41:48 +0000

    fizzbuzz in py

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,8 +1,13 @@
 
-i = int()
-
-for i=0;i<=50;i++
+i = 0
+while i<=50:
     
-if i%3==0 and i%5==0
-
-elif i%3==0
+    i+=1
+    if(i%3==0 and i%5==0):
+        print("fizzbuzz")
+    elif i%3==0:
+        print("fizz")
+    elif i%5==0:
+        print("buzz")
+    else:
+        print(i)
