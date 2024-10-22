5ccf39c84c4edf1ede545921282c583b6352e857
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-22 01:42:00 +0000

    updated cal in py once again

--- functions/update_budget.py
+++ functions/update_budget.py
@@ -1,37 +1,27 @@
 
 def user_inputs(type):
-    return float(input(f"Monthly {type}"))
+    return float(input(f"Monthly {type} "))
 
-def divide(type, income):
-    return type/ income *100
 
-    
+income = user_inputs("monthly income:\n")
+rent = user_inputs("monthly rent:\n")
+utilities = user_inputs("monthly utilities cost:\n")
+groceries = user_inputs("monthly groceries cost:\n")
+transportation = user_inputs("monthly transportation cost:\n")
 
-income = user_inputs("what is your monthly income?:\n")
-rent = user_inputs("what is your monthly rent?:\n")
-utilities = user_inputs("what is your monthly cost for utilities?:\n")
-groceries = user_inputs("what is your monthly cost for groceries?:\n")
-transportation = user_inputs("what is your monthly cost for transportation?:\n")
-savings = income*0.2
+savings = income *0.2
 expenses = rent + utilities + groceries + transportation
 spend = income-int(expenses)-savings
 
-
-def input_value(type, amount):
-    val = input()
-
-    return f"what is your monthly {type}?"
-
-
 def percent(type, amount):
-    per = amount/income *100
+    per = amount / income * 100    
 
-    return f"Your {type} is {per}% income."
+    return f"Your {type} is {per}% income\n"
 
-print(f"your monthly income is {income: .2f}")
-print(f"your monthly expenses is {expenses: .2f}")
-print(f"your monthly savings is {savings: .2f}")
-print(f"your monthly spending money is {spend: .2f}")
+print(f"your monthly income is ${income: .2f}\n")
+print(f"your monthly expenses is ${expenses: .2f}\n")
+print(f"your monthly savings is ${savings: .2f}\n")
+print(f"your monthly spending is ${spend: .2f}\n")
 print(percent("rent", rent))
 print(percent("utilities", utilities))
 print(percent("groceries", groceries))
