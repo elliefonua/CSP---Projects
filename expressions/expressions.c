e1208ddc3e524916da2238d55c6033dd82fca462
elliefonua <ellie.fonua@ucas-edu.net>
2024-10-22 02:12:49 +0000

    expression in c .2

--- expressions/expressions.c
+++ expressions/expressions.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     int a, b, c, d, e;
     a = 2;
     b = 4;
     c = 5;
     d = 3;
     e = 6;
-    printf("%d", (int)7 - (int)24 / (int)8 * (int)b + (int)e);
-    printf("%d", (int)18 / (int)d - (int)7 + (int)2 * (int)c);
-    printf("%d", (int)e * (int)b / (int)12 + (int)72 / (int)8 - (int)9);
-    printf("%d", (int)(17 - (int)e / (int)a) + (int)b * (int)d);
-    printf("%d", (int)-2 * (int)(1 * (int)b - (int)a / (int)a) + (int)(e + (int)a - (int)d));
-    printf("%d", (int)-1 * (int)((3 - (int)b * (int)7) / (int)c) - (int)(a * (int)24 / (int)e)));
-    printf("%d", (int) (d * (int)c ^ (int)a / (int)15) - (int) (c - (int)a ^ (int)a));
-    printf("%d", (int) (1 ^ (int)b * (int)a ^ (int)a + (int)d ^ (int)d) - (int)a ^ (int)c / (int)b);
-    printf("%d", (int)(22 / (int)a - (float)2.5) ^ (int)a + (int)(b - (int)e / (int)e) ^ (int)a);
+
+    printf("%d\n", 7 - 24 / 8 * b + e);
+    printf("%d\n", 18 / d - 7 + 2 * c);
+    printf("%d\n", e * b / 12 + 72 / 8 - 9);
+    printf("%d\n", 17 - e / a + b * d);
+    printf("%d\n", -2 * (1 * b - a / a) + (e + a - d));
+    printf("%d\n", -1 * ((3 - b * 7) / c - (a * 24 / e)));
+    printf("%d\n", d * c ^ a / 15 - (c - a ^ a));
+    printf("%d\n", (1 ^ b * a ^ a + d ^ d) - a ^ c / b);
+
     return 0;
 }
